dc545444ef3679af819cff3cae92edd22973672f
0x5A4159 <zyates001@gmail.com>
2023-05-19 12:06:59 -0400

    created path builder

---
+++ src/pre_boot.rs
@@ -0,0 +1,31 @@
+use std::io::Write;
+use std::fs;
+
+fn file_check(file_name: &str, content: &str) {
+    if !(std::path::Path::new(file_name).exists()) {
+        fs::write(file_name, content)
+            .expect("No file permissions to write");
+        println!("+ File {file_name} created!")
+    }
+
+}
+
+fn dir_check(dir_name: &str) {
+    if !(std::path::Path::new(dir_name).is_dir()) {
+        fs::create_dir(dir_name)
+            .expect("No file permissions to make directory");
+        println!("+ Directory {dir_name} created")
+    }
+}
+
+pub fn preload() {
+    println!("Beginning setup, checking for files");
+    // This can be done better, I just know it, but while the project size is small this will build the paths fine.
+    dir_check("server");
+    dir_check("server/server_content");
+    file_check("./server/server_content/index.html", "<!DOCTYPE html> <html lang=\"en\"> <head> <style> body { background-color: whitesmoke; } .Main { -ms-overflow-style: none; scrollbar-width: none; margin-top:0; padding-top:0; position:fixed; overflow-y: scroll; height:100%; width:66%; top: 50%; left: 50%; transform:translate(-50%,-50%); padding-left:20px; padding-right:20px; background-color:dimgray; } .Main::-webkit-scrollbar{ display:none; } .Main #Header { margin-top:0.2em; font-size: 100px; width:4em; color: whitesmoke; background-color: #234f4b; position: relative; text-align: center; left:50%; transform: translate(-50%, 0); border-radius: 10px; padding:10px; text-shadow: 0px 6px black; font-family: 'DejaVu Sans Mono'; } p { font-size: 25px; color: whitesmoke; text-shadow: 0px 2px black; font-family: 'DejaVu Sans Mono'; } </style> </head> <body> <div class=\"Main\"> <h1 id=\"Header\">Zimple</h1> <p>Zimple is a simple light-weight HTTP server, primarily dedicated towards localhost traffic.</p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p> </div> </body> </html>");
+    file_check("./server/server_content/404.html","<h1>ERROR 404</h1><p>Not Found</p>");
+    file_check("./server/server_content/favicon.ico","");
+    file_check("./server/config.cfg","ServerIP=localhost\nServerPort=8080\nHeaderSize=1024");
+    println!("Config files exist\nCan begin run server attempt.");
+}
